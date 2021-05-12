48ead10b699de263dfaa9dc357375a9d442c6a35
Luca <luca.biotti.flores@gmail.com>
2021-05-12 18:53:03 -0400

    se añade Tooltips, modales dentro de las cards, cambio de color del navbar cuando cuesta leerlo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,51 @@
 $(function(){
 
+    $("a").click(function(event){
+        if (this.hash !== "") {
+            event.preventDefault();
+ 
+            var hash = this.hash;
+ 
+            $("html, body").animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+     });
 
+//enable tooltips everywhere
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  
+//Cambio color navbar
+$(window).scroll(function(){
+    if ($("#navbarpulento").offset().top > 2000){
+        $("#navbarpulento").addClass("bg-dark");
+    }else {
+        $("#navbarpulento").removeClass("bg-dark");
+    }
+    });
 
+//Modal
 
+var modal = document.getElementById("myModal");
 
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
 
+var span = document.getElementsByClassName("close")[0];
 
-
-
-
+span.onclick = function() { 
+    modal.style.display = "none";
+  }
 
 });
